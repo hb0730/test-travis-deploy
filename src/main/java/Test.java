7d875ebdb155d3737f666aa41bde96314f5fde41
黄兵 <1278032416@qq.com>
2020-08-07 09:29:22 +0800

    update

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author bing_huang
+ * @date 2020/08/07 9:28
+ * @since V1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
