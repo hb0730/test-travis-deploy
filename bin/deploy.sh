76afcd3580afcf426896a6a552ef9f680455809d
黄兵 <1278032416@qq.com>
2020-08-11 09:50:48 +0800

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,8 +5,8 @@ if [ -z $TRAVIS_TAG ]; then
   echo "tag is null"
 else
   echo "Publishing Maven snapshot..."
-  openssl aes-256-cbc -K $encrypted_81c98acad902_key -iv $encrypted_81c98acad902_iv -in bin/signingkey.asc.enc -out bin/codesigning.asc -d
-  gpg --fast-import bin/signingkey.asc
+  openssl aes-256-cbc -K $encrypted_81c98acad902_key -iv $encrypted_81c98acad902_iv -in bin/codesigning.asc.enc -out bin/codesigning.asc -d
+  gpg --fast-import bin/codesigning.asc
 
   mvn clean deploy --settings=bin/settings.xml -DskipTests=true
 fi
